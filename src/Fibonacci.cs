e3b727591ce95a02fb856bb24a6bdabef5a4b4ec
Afner <aperales@hotmail.com>
2015-09-23 11:01:54 -0500

    Fibonacci method and tester

--- src/Fibonacci.cs
+++ src/Fibonacci.cs
@@ -2,5 +2,20 @@
 {
     public class Fibonacci
     {
+        int n=0;
+        public int FibonMethod(int n)
+        {
+
+            int number = n; //Need to decrement by 1 since we are starting from 0
+            int[] Fib = new int[number + 1];
+            Fib[0] = 0;
+            Fib[1] = 1;
+
+            for (int i = 2; i <= number; i++)
+            {
+                Fib[i] = Fib[i - 2] + Fib[i - 1];
+            }
+            return Fib[number];
+        }
     }
 }
--- tests/Fibonacci.Tests.cs
+++ tests/Fibonacci.Tests.cs
@@ -1,6 +1,19 @@
 ﻿namespace CSharp.Training.Tests
 {
+    using CSharp.Training;
+    using NUnit.Framework;
+    [TestFixture]
     public class FibonacciTests
     {
+        
+        Fibonacci Test = new Fibonacci();
+        [Test]
+        public void TestingFibonacci(){
+       
+            Assert.That(Test.FibonMethod(4),Is.EqualTo(3));
+            Assert.That(Test.FibonMethod(5), Is.EqualTo(5));
+            Assert.That(Test.FibonMethod(2), Is.EqualTo(1));
+        }
+
     }
 }
