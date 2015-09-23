2dfc9260fc973ec5c0af92ea345ac97c5f3485e3
Afner <aperales@hotmail.com>
2015-09-23 12:21:07 -0500

    Finished Perfect Number and Test

--- src/PerfectNumber.cs
+++ src/PerfectNumber.cs
@@ -1,7 +1,26 @@
 ﻿namespace CSharp.Training
 {
-    public class PerfectNumberExtention
+    public class PerfectNumber
     {
+        public string PerfectNumberMethod(int number)
+        {
+            int sum = 0;
+            for (int i = 1; i < number; i++)
+            {
+                if (number % i == 0)
+                {
+                    sum = sum + i;
+                }
+            }
+            if (sum == number)
+            {
+                return "Perfect number";
+            }
+            else
+            {
+                return "Not Perfect number";
+            }
+        }
         
     }
 }
--- tests/PerfectNumberTest.cs
+++ tests/PerfectNumberTest.cs
@@ -1,6 +1,18 @@
 ﻿namespace CSharp.Training.Tests
 {
+    using CSharp.Training;
+    using NUnit.Framework;
+    [TestFixture]
     public class PerfectNumberTest
     {
+        PerfectNumber Test = new PerfectNumber();
+        [Test]
+        public void TestingPerfectNumber()
+        {
+            Assert.That(Test.PerfectNumberMethod(6), Is.EqualTo("Perfect number"));
+            Assert.That(Test.PerfectNumberMethod(15), Is.EqualTo("Not Perfect number"));
+            Assert.That(Test.PerfectNumberMethod(28), Is.EqualTo("Perfect number"));
+            Assert.That(Test.PerfectNumberMethod(39), Is.EqualTo("Not Perfect number"));
+        }
     }
 }
