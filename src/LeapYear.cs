5dcc02f8cc4e7b851cdd4da2402e9917ec26b313
Afner <aperales@hotmail.com>
2015-09-23 11:55:33 -0500

    Finished LeapYear and Test

--- src/LeapYear.cs
+++ src/LeapYear.cs
@@ -2,5 +2,16 @@
 {
     public class LeapYear
     {
+        public string LeapYearMethod(int year)
+        { 
+            if ( year%400 == 0)
+            return "LeapYear";
+            else if ( year%100 == 0)
+            return "Not LeapYear";
+            else if ( year%4 == 0 )
+            return "LeapYear";
+            else
+            return "Not LeapYear";
+        }
     }
 }
--- tests/LeapYear.Test.cs
+++ tests/LeapYear.Test.cs
@@ -1,6 +1,20 @@
 ﻿namespace CSharp.Training.Tests
 {
-    public class LeapYear
+    using CSharp.Training;
+    using NUnit.Framework;
+    [TestFixture]
+    public class LeapYearTests
     {
+        
+            LeapYear Test = new LeapYear();
+            [Test]
+            public void TestingLeapYear()
+            {
+                Assert.That(Test.LeapYearMethod(400), Is.EqualTo("LeapYear"));
+                Assert.That(Test.LeapYearMethod(100), Is.EqualTo("Not LeapYear"));
+                Assert.That(Test.LeapYearMethod(1992), Is.EqualTo("LeapYear"));
+                Assert.That(Test.LeapYearMethod(1867), Is.EqualTo("Not LeapYear"));
+            }
+        
     }
 }
