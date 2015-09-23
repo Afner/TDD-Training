a9e22ec4b82e163f106e5fa23314a5ce92672d1a
Afner <aperales@hotmail.com>
2015-09-23 11:14:28 -0500

    Finished Fibonacci method and tester

--- src/Fibonacci.cs
+++ src/Fibonacci.cs
@@ -7,15 +7,28 @@
         {
 
             int number = n; //Need to decrement by 1 since we are starting from 0
+            if (n == 0)
+            {
+                return 0;
+            }
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            { 
             int[] Fib = new int[number + 1];
             Fib[0] = 0;
             Fib[1] = 1;
-
+            
             for (int i = 2; i <= number; i++)
             {
+               
                 Fib[i] = Fib[i - 2] + Fib[i - 1];
-            }
+                }
+           
             return Fib[number];
+            }
         }
     }
 }
--- tests/Fibonacci.Tests.cs
+++ tests/Fibonacci.Tests.cs
@@ -12,7 +12,8 @@
        
             Assert.That(Test.FibonMethod(4),Is.EqualTo(3));
             Assert.That(Test.FibonMethod(5), Is.EqualTo(5));
-            Assert.That(Test.FibonMethod(2), Is.EqualTo(1));
+            Assert.That(Test.FibonMethod(0), Is.EqualTo(0));
+            Assert.That(Test.FibonMethod(1), Is.EqualTo(1));
         }
 
     }
