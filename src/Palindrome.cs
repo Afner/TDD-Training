e396352b08c591798c3200d62caebf8f081e4330
Afner <aperales@hotmail.com>
2015-09-23 12:10:55 -0500

    Finished Palindrome and Test

--- src/Palindrome.cs
+++ src/Palindrome.cs
@@ -1,6 +1,24 @@
 ﻿namespace CSharp.Training
 {
-    public static class Palindrome
+    public class Palindrome
     {
+       
+        public string PalindromeMethod(string word)
+        {
+            string revs = "";
+            for (int i = word.Length - 1; i >= 0; i--) //String Reverse
+            {
+                revs += word[i].ToString();
+            }
+            if (revs == word) // Checking whether string is palindrome or not
+            {
+                return "Is a palindrome";
+            }
+            else
+            {
+                return "Is not a palindrome";
+            }
+        }
+
     }
 }
--- tests/PalindromeTest.cs
+++ tests/PalindromeTest.cs
@@ -1,6 +1,19 @@
 ﻿namespace CSharp.Training.Tests
 {
+    using CSharp.Training;
+    using NUnit.Framework;
+    [TestFixture]
     public class PalindromeTest
     {
+        Palindrome Test = new Palindrome();
+        [Test]
+        public void TestingPalindrome()
+        {
+            Assert.That(Test.PalindromeMethod("Hola"), Is.EqualTo("Is not a palindrome"));
+            Assert.That(Test.PalindromeMethod("anitalavalatina"), Is.EqualTo("Is a palindrome"));
+            Assert.That(Test.PalindromeMethod("ola ke ase"), Is.EqualTo("Is not a palindrome"));
+            Assert.That(Test.PalindromeMethod("ingirumimusnocteetconsumimurigni"), Is.EqualTo("Is a palindrome"));
+      
+        }
     }
 }
